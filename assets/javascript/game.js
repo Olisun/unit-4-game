aad4c8e18db621503378d19c49774f7272723039
Oliver Sun <olisun@mac.com>
2019-08-31 23:47:26 -0700

    added new game function in js, stylings in css and a button to start new game in html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,48 @@
 $(document).ready(function() {
-
   var ruby = $('#ruby');
   var diamond = $('#diamond');
   var yellowThingy = $('#yellowThingy');
   var emerald = $('#emerald');
+  var computerRandomNum = $('#random-number');
+  var resetGame = $('#reset')
 
-  ruby.on("click", function() {
-    alert('ruby has been clicked')
-  });
 
-  diamond.on("click", function() {
-    alert('diamond has been clicked')
-  });
+  function playGame() {
+    var randomNum = ['1', '2', '3', '4', '5', '6', '7'];
+    var computerGuess = randomNum[Math.floor(Math.random() * randomNum.length)];
+    var numTest = document.getElementById('random-number');
 
-  yellowThingy.on("click", function() {
-    alert('yellowThingy has been clicked')
-  });
+    numTest.innerHTML = computerGuess;
 
-  emerald.on("click", function() {
-    alert('emerald has been clicked')
-  });
+    ruby.on("click", function() {
+      alert('ruby has been clicked')
+    });
+
+    diamond.on("click", function() {
+      alert('diamond has been clicked')
+    });
+
+    yellowThingy.on("click", function() {
+      alert('yellowThingy has been clicked')
+    });
+
+    emerald.on("click", function() {
+      alert('emerald has been clicked')
+    });
+  }
+
+  function newGame() {
+    resetGame.on('click', function() {
+      var randomNum = ['1', '2', '3', '4', '5', '6', '7'];
+      var computerGuess = randomNum[Math.floor(Math.random() * randomNum.length)];
+      var numTest = document.getElementById('random-number');
+
+      numTest.innerHTML = computerGuess;
+    })
+  }
+
+  playGame();
+  newGame();
 
 
 });
