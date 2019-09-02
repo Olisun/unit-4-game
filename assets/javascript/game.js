a3b3aa4d30e65442232aeba7973808f67178b138
Oliver Sun <olisun@mac.com>
2019-09-02 13:17:53 -0700

    added comments to the js file going over all the logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,18 @@
   $(document).ready(function() {
+    // Declaring global variables.
     var ruby = $('#ruby');
     var diamond = $('#diamond');
     var yellowThingy = $('#yellowThingy');
     var emerald = $('#emerald');
     var resetGame = $('#reset');
 
-
+    // This function starts the inital game.
     function playGame() {
+      // Using our friend math.floor-math.round to have the computer select a random number for the user to try and match.
       var computerNum = [19, 25, 33, 38, 45, 50, 53, 61, 69, 72, 75, 84, 86, 94, 99, 105, 111, 120];
       var computerGuess = computerNum[Math.floor(Math.random() * computerNum.length)];
       var computerNumDom = $('#random-number');
-
+      // Assinging a random point value to each crystal at the start of each game.
       var rubyNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
       var rubyGuess = rubyNum[Math.floor(Math.random() * rubyNum.length)];
       var rubyNumDom = $('#r-num-test');
@@ -32,15 +34,20 @@
       var totalScoreDom = $('#total-score');
       var restart = 0;
 
+      // This sets the "Your Total Score Is:" from the DOM at 0 at the start of each game.
       totalScoreDom.text(restart);
+      // This sets the "Wins:" from the DOM at 0 at the start of each game.
       wins.text(restart);
+      // This sets the "Losses:" from the DOM at 0 at the start of each game.
       losses.text(restart);
+      // These set the random numbers chosen from arrays above to the selected targets on the DOM. I added the crystals' numbers so I can reference them when building. I sill change the css display to none when deploying the final product so the user won't see them.
       computerNumDom.text(computerGuess);
       rubyNumDom.text(rubyGuess);
       diamondNumDom.text(diamondGuess);
       yellowNumDom.text(yellowGuess);
       emeraldNumDom.text(emeraldGuess);
 
+      // Onclick functions that add the point values assigned to each crystal to the total score counter.
       ruby.on("click", function() {
         totalScoreDom.text(restart += rubyGuess);
       });
@@ -66,6 +73,7 @@
       }
     }
 
+    // function creates a new game after the user clicks on the reset button.
     function newGame() {
       resetGame.on('click', function() {
         var computerNum = [19, 25, 33, 38, 45, 50, 53, 61, 69, 72, 75, 84, 86, 94, 99, 105, 111, 120];
@@ -129,6 +137,7 @@
       })
     }
 
+    // Finally, calling the two game functions to activate. The first activates the game when the page loads and the second activates the game after the user clicks the reset button
     playGame();
     newGame();
 
