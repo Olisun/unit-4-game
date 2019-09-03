2187ea2950f620e7058f3e484b081c630451c0be
Oliver Sun <olisun@mac.com>
2019-09-03 12:52:05 -0700

    updated READMe with screen-shot and methods used to build the game

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# unit-4-game
-j-Query Homework
+# Unit-4-Game
+
+## Page preview: 
+![](assets/images/screen-shot.png)
+
+
+## About the project:
+
+We had to build a game where the user clicks on one of 4 gems, each assigned with a random point value at the beginning of the game. Each click adds up the points in a total score box. The user must match the total score with a randomly chosen number shown at the beginning of the game. If they hit that number they win and if they go over they lose. They can't see what each gem is worth so the object is to keep track of each gem's point value in order to add up their total score exactly to match the random number. Wins and losses are tracked
+
+
+## Techologies used to build:
+  * HTML
+  * CSS
+  * jQuery
+  * Javascript
+
+## Methodology:
+
+I created sepearate arrays for the random number shown to the user as well as the gems. Using our friend math.floor-math.round, I got the random numbers for the game target and the point values for each gem. From their, I mapped the random numbers to their respective fields. My logic is detailed in the comments in the game2.js. 
+
+The original game.js had to be refactored in a big way because I ran into major bugs with the score counts. They were not adding up correctly so I went back and reviewed the class jQuery resources from last week. I realized that the numbers were most likely string values. I started a new js file to refactor the game code. 
+
+I built the page structure in HTML by taking separate screen-shots of the gems plus the back ground from the screen-shot given to us in the homework instructions. I used the gem screen-shots to create buttons for the gems. I used css to place the images accordingly on the page.
+
+
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,181 +1,184 @@
-$(document).ready(function() {
-  // Declaring global variables.
-  var ruby = $('#ruby');
-  var diamond = $('#diamond');
-  var yellowThingy = $('#yellowThingy');
-  var emerald = $('#emerald');
-  var resetGame = $('#reset');
-
-  // This function starts the inital game.
-  function playGame() {
-    // Using our friend math.floor-math.round to have the computer select a random number for the user to try and match.
-    var computerNum = [19, 25, 33, 38, 45, 50, 53, 61, 69, 72, 75, 84, 86, 94, 99, 105, 111, 120];
-    var computerGuess = computerNum[Math.floor(Math.random() * computerNum.length)];
-    var computerNumDom = $('#random-number');
-    // Assinging a random point value to each crystal at the start of each game.
-    var rubyNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-    var rubyGuess = rubyNum[Math.floor(Math.random() * rubyNum.length)];
-    var rubyNumDom = $('#r-num-test');
-
-    var diamondNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-    var diamondGuess = diamondNum[Math.floor(Math.random() * diamondNum.length)];
-    var diamondNumDom = $('#d-num-test');
-
-    var yellowNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-    var yellowGuess = rubyNum[Math.floor(Math.random() * yellowNum.length)];
-    var yellowNumDom = $('#y-num-test');
-
-    var emeraldNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-    var emeraldGuess = rubyNum[Math.floor(Math.random() * emeraldNum.length)];
-    var emeraldNumDom = $('#e-num-test');
-
-    var wins = $('#wins-placeholder');
-    var losses = $('#losses-placeholder');
-    var totalScoreDom = $('#total-score');
-    var pointsForWin = 1;
-    var pointsForLosses = 1;
-    var restart = 0;
-    var counter = 0;
+// Major refactor to the code below. See game2.js.
 
 
-    // This sets the "Your Total Score Is:" from the DOM at 0 at the start of each game.
-    totalScoreDom.text(restart);
-    // This sets the "Wins:" from the DOM at 0 at the start of each game.
-    // wins.text(restart);
-    // This sets the "Losses:" from the DOM at 0 at the start of each game.
-    // losses.text(restart);
-    // These set the random numbers chosen from arrays above to the selected targets on the DOM. I added the crystals' numbers so I can reference them when building. I sill change the css display to none when deploying the final product so the user won't see them.
-    computerNumDom.text(computerGuess);
-    rubyNumDom.text(rubyGuess);
-    diamondNumDom.text(diamondGuess);
-    yellowNumDom.text(yellowGuess);
-    emeraldNumDom.text(emeraldGuess);
+// $(document).ready(function() {
+//   // Declaring global variables.
+//   var ruby = $('#ruby');
+//   var diamond = $('#diamond');
+//   var yellowThingy = $('#yellowThingy');
+//   var emerald = $('#emerald');
+//   var resetGame = $('#reset');
+
+//   // This function starts the inital game.
+//   function playGame() {
+//     // Using our friend math.floor-math.round to have the computer select a random number for the user to try and match.
+//     var computerNum = [19, 25, 33, 38, 45, 50, 53, 61, 69, 72, 75, 84, 86, 94, 99, 105, 111, 120];
+//     var computerGuess = computerNum[Math.floor(Math.random() * computerNum.length)];
+//     var computerNumDom = $('#random-number');
+//     // Assinging a random point value to each crystal at the start of each game.
+//     var rubyNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+//     var rubyGuess = rubyNum[Math.floor(Math.random() * rubyNum.length)];
+//     var rubyNumDom = $('#r-num-test');
+
+//     var diamondNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+//     var diamondGuess = diamondNum[Math.floor(Math.random() * diamondNum.length)];
+//     var diamondNumDom = $('#d-num-test');
+
+//     var yellowNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+//     var yellowGuess = rubyNum[Math.floor(Math.random() * yellowNum.length)];
+//     var yellowNumDom = $('#y-num-test');
+
+//     var emeraldNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+//     var emeraldGuess = rubyNum[Math.floor(Math.random() * emeraldNum.length)];
+//     var emeraldNumDom = $('#e-num-test');
+
+//     var wins = $('#wins-placeholder');
+//     var losses = $('#losses-placeholder');
+//     var totalScoreDom = $('#total-score');
+//     var pointsForWin = 1;
+//     var pointsForLosses = 1;
+//     var restart = 0;
+//     var counter = 0;
 
 
-    // Onclick functions that add the point values assigned to each crystal to the total score counter.
-    ruby.on("click", function() {
-      totalScoreDom.text(restart += rubyGuess);
-      counter += rubyGuess;
-      wins.text(counter);
-      document.getElementById('counter-test').innerText = counter;
-    });
-
-    diamond.on("click", function() {
-      totalScoreDom.text(restart += diamondGuess);
-      counter += diamondGuess;
-      wins.text(counter);
-      document.getElementById('counter-test').innerText = counter;
-    });
-
-    yellowThingy.on("click", function() {
-      totalScoreDom.text(restart += yellowGuess);
-      counter += yellowGuess;
-      wins.text(counter);
-      document.getElementById('counter-test').innerText = counter;
-    });
-
-    emerald.on("click", function() {
-      totalScoreDom.text(restart += emeraldGuess);
-      counter += emeraldGuess;
-      wins.text(counter);
-      document.getElementById('counter-test').innerText = counter;
-    });
-
-    if (typeof counter == 'number') {
-      alert('true')
-    } else {
-      alert('false')
-    };
-
-    if (counter.value > computerGuess) {
-      alert('you lose')
-    }
-  }
+//     // This sets the "Your Total Score Is:" from the DOM at 0 at the start of each game.
+//     totalScoreDom.text(restart);
+//     // This sets the "Wins:" from the DOM at 0 at the start of each game.
+//     // wins.text(restart);
+//     // This sets the "Losses:" from the DOM at 0 at the start of each game.
+//     // losses.text(restart);
+//     // These set the random numbers chosen from arrays above to the selected targets on the DOM. I added the crystals' numbers so I can reference them when building. I sill change the css display to none when deploying the final product so the user won't see them.
+//     computerNumDom.text(computerGuess);
+//     rubyNumDom.text(rubyGuess);
+//     diamondNumDom.text(diamondGuess);
+//     yellowNumDom.text(yellowGuess);
+//     emeraldNumDom.text(emeraldGuess);
 
 
+//     // Onclick functions that add the point values assigned to each crystal to the total score counter.
+//     ruby.on("click", function() {
+//       totalScoreDom.text(restart += rubyGuess);
+//       counter += rubyGuess;
+//       wins.text(counter);
+//       document.getElementById('counter-test').innerText = counter;
+//     });
 
-  // function creates a new game after the user clicks on the reset button.
-  function newGame() {
-    resetGame.on('click', function() {
-      var computerNum = [19, 25, 33, 38, 45, 50, 53, 61, 69, 72, 75, 84, 86, 94, 99, 105, 111, 120];
-      var computerGuess = computerNum[Math.floor(Math.random() * computerNum.length)];
-      var computerNumDom = $('#random-number');
+//     diamond.on("click", function() {
+//       totalScoreDom.text(restart += diamondGuess);
+//       counter += diamondGuess;
+//       wins.text(counter);
+//       document.getElementById('counter-test').innerText = counter;
+//     });
 
-      var rubyNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-      var rubyGuess = rubyNum[Math.floor(Math.random() * rubyNum.length)];
-      var rubyNumDom = $('#r-num-test');
+//     yellowThingy.on("click", function() {
+//       totalScoreDom.text(restart += yellowGuess);
+//       counter += yellowGuess;
+//       wins.text(counter);
+//       document.getElementById('counter-test').innerText = counter;
+//     });
 
-      var diamondNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-      var diamondGuess = diamondNum[Math.floor(Math.random() * diamondNum.length)];
-      var diamondNumDom = $('#d-num-test');
+//     emerald.on("click", function() {
+//       totalScoreDom.text(restart += emeraldGuess);
+//       counter += emeraldGuess;
+//       wins.text(counter);
+//       document.getElementById('counter-test').innerText = counter;
+//     });
 
-      var yellowNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-      var yellowGuess = rubyNum[Math.floor(Math.random() * yellowNum.length)];
-      var yellowNumDom = $('#y-num-test');
+//     if (typeof counter == 'number') {
+//       alert('true')
+//     } else {
+//       alert('false')
+//     };
 
-      var emeraldNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-      var emeraldGuess = rubyNum[Math.floor(Math.random() * emeraldNum.length)];
-      var emeraldNumDom = $('#e-num-test');
-
-      var wins = $('#wins-placeholder');
-      var losses = $('#losses-placeholder');
-      var totalScoreDom = $('#total-score');
-      var pointsForWin = 1;
-      var pointsForLosses = 1;
-      var restart = 0;
-      var counter = 0;
+//     if (counter.value > computerGuess) {
+//       alert('you lose')
+//     }
+//   }
 
 
-      wins.text(restart);
-      losses.text(restart);
-      computerNumDom.text(computerGuess);
-      rubyNumDom.text(rubyGuess);
-      diamondNumDom.text(diamondGuess);
-      yellowNumDom.text(yellowGuess);
-      emeraldNumDom.text(emeraldGuess);
 
-      ruby.on("click", function() {
-        totalScoreDom.text(restart += rubyGuess);
-        counter += rubyGuess;
-        wins.text(counter);
-        document.getElementById('counter-test').innerText = counter;
-      });
+//   // function creates a new game after the user clicks on the reset button.
+//   function newGame() {
+//     resetGame.on('click', function() {
+//       var computerNum = [19, 25, 33, 38, 45, 50, 53, 61, 69, 72, 75, 84, 86, 94, 99, 105, 111, 120];
+//       var computerGuess = computerNum[Math.floor(Math.random() * computerNum.length)];
+//       var computerNumDom = $('#random-number');
 
-      diamond.on("click", function() {
-        totalScoreDom.text(restart += diamondGuess);
-        counter += diamondGuess;
-        wins.text(counter);
-        document.getElementById('counter-test').innerText = counter;
-      });
+//       var rubyNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+//       var rubyGuess = rubyNum[Math.floor(Math.random() * rubyNum.length)];
+//       var rubyNumDom = $('#r-num-test');
 
-      yellowThingy.on("click", function() {
-        totalScoreDom.text(restart += yellowGuess);
-        counter += yellowGuess;
-        wins.text(counter);
-        document.getElementById('counter-test').innerText = counter;
-      });
+//       var diamondNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+//       var diamondGuess = diamondNum[Math.floor(Math.random() * diamondNum.length)];
+//       var diamondNumDom = $('#d-num-test');
 
-      emerald.on("click", function() {
-        totalScoreDom.text(restart += emeraldGuess);
-        counter += emeraldGuess;
-        wins.text(counter);
-        document.getElementById('counter-test').innerText = counter;
-      });
+//       var yellowNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+//       var yellowGuess = rubyNum[Math.floor(Math.random() * yellowNum.length)];
+//       var yellowNumDom = $('#y-num-test');
 
-      // if (counter < 10) {
-      //   alert('you win!')
-      // }
+//       var emeraldNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+//       var emeraldGuess = rubyNum[Math.floor(Math.random() * emeraldNum.length)];
+//       var emeraldNumDom = $('#e-num-test');
 
-      if (typeof counter == 'number') {
-        alert('true')
-      } else {
-        alert('false')
-      };
-    })
-  }
+//       var wins = $('#wins-placeholder');
+//       var losses = $('#losses-placeholder');
+//       var totalScoreDom = $('#total-score');
+//       var pointsForWin = 1;
+//       var pointsForLosses = 1;
+//       var restart = 0;
+//       var counter = 0;
 
-  // Finally, calling the two game functions to activate. The first activates the game when the page loads and the second activates the game after the user clicks the reset button
-  playGame();
-  newGame();
 
-});
+//       wins.text(restart);
+//       losses.text(restart);
+//       computerNumDom.text(computerGuess);
+//       rubyNumDom.text(rubyGuess);
+//       diamondNumDom.text(diamondGuess);
+//       yellowNumDom.text(yellowGuess);
+//       emeraldNumDom.text(emeraldGuess);
+
+//       ruby.on("click", function() {
+//         totalScoreDom.text(restart += rubyGuess);
+//         counter += rubyGuess;
+//         wins.text(counter);
+//         document.getElementById('counter-test').innerText = counter;
+//       });
+
+//       diamond.on("click", function() {
+//         totalScoreDom.text(restart += diamondGuess);
+//         counter += diamondGuess;
+//         wins.text(counter);
+//         document.getElementById('counter-test').innerText = counter;
+//       });
+
+//       yellowThingy.on("click", function() {
+//         totalScoreDom.text(restart += yellowGuess);
+//         counter += yellowGuess;
+//         wins.text(counter);
+//         document.getElementById('counter-test').innerText = counter;
+//       });
+
+//       emerald.on("click", function() {
+//         totalScoreDom.text(restart += emeraldGuess);
+//         counter += emeraldGuess;
+//         wins.text(counter);
+//         document.getElementById('counter-test').innerText = counter;
+//       });
+
+//       // if (counter < 10) {
+//       //   alert('you win!')
+//       // }
+
+//       if (typeof counter == 'number') {
+//         alert('true')
+//       } else {
+//         alert('false')
+//       };
+//     })
+//   }
+
+//   // Finally, calling the two game functions to activate. The first activates the game when the page loads and the second activates the game after the user clicks the reset button
+//   playGame();
+//   newGame();
+
+// });
