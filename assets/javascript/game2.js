11cbf30866ac084a99e3030309c41a338f94447f
Oliver Sun <olisun@mac.com>
2019-09-03 17:39:21 -0700

    fixed bug in game2.js for keeping score of wins/losses. one bug left to go but it's a major one >:-(.

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,60 +1,57 @@
 $(document).ready(function() {
+  // Using our friend math.floor-math.round to have the computer select a random number for the user to try and match.
+  var computerNum = [19, 25, 33, 38, 45, 50, 53, 61, 69, 72, 75, 84, 86, 94, 99, 105, 111, 120];
+  var computerGuess = computerNum[Math.floor(Math.random() * computerNum.length)];
+  var computerNumDom = $('#random-number');
+
+  // Assinging a random point value to each crystal at the start of each game.
+  var rubyNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  var rubyGuess = rubyNum[Math.floor(Math.random() * rubyNum.length)];
+  var rubyNumDom = $('#r-num-test'); // --> This sets me up to show the gem point value for reference during developement. Will hide when game ios deployed.
+
+  var diamondNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  var diamondGuess = diamondNum[Math.floor(Math.random() * diamondNum.length)];
+  var diamondNumDom = $('#d-num-test'); // --> This sets me up to show the gem point value for reference during developement. Will hide when game ios deployed.
+
+  var yellowNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  var yellowGuess = rubyNum[Math.floor(Math.random() * yellowNum.length)];
+  var yellowNumDom = $('#y-num-test'); // --> This sets me up to show the gem point value for reference during developement. Will hide when game ios deployed.
+
+  var emeraldNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  var emeraldGuess = rubyNum[Math.floor(Math.random() * emeraldNum.length)];
+  var emeraldNumDom = $('#e-num-test'); // --> This sets me up to show the gem point value for reference during developement. Will hide when game ios deployed.
+
   // Declaring global variables.
   var ruby = $('#ruby');
   var diamond = $('#diamond');
   var yellowThingy = $('#yellowThingy');
   var emerald = $('#emerald');
   var resetGame = $('#reset');
+  var wins = $('#wins-placeholder');
+  var losses = $('#losses-placeholder');
+  var totalScoreDom = $('#total-score');
+  var winPoints = 0;
+  var lossPoints = 0;
+  var start = 0;
+  var counter = 0;
+  var targetNumber = computerGuess
+
+
+  // This sets the "Your Total Score Is:" from the DOM at 0 at the start of each game.
+  totalScoreDom.text(start);
+  // This sets the "Wins:" from the DOM at 0 at the start of each game.
+  wins.text(start);
+  // This sets the "Losses:" from the DOM at 0 at the start of each game.
+  losses.text(start);
+  // These set the random numbers chosen from arrays above to the selected targets on the DOM. I added the crystals' numbers so I can reference them when building. I sill change the css display to none when deploying the final product so the user won't see them.
+  computerNumDom.text(targetNumber);
+  rubyNumDom.text(rubyGuess);
+  diamondNumDom.text(diamondGuess);
+  yellowNumDom.text(yellowGuess);
+  emeraldNumDom.text(emeraldGuess);
 
   // This function starts the inital game.
   function playGame() {
-
-    // Using our friend math.floor-math.round to have the computer select a random number for the user to try and match.
-    var computerNum = [19, 25, 33, 38, 45, 50, 53, 61, 69, 72, 75, 84, 86, 94, 99, 105, 111, 120];
-    var computerGuess = computerNum[Math.floor(Math.random() * computerNum.length)];
-    var computerNumDom = $('#random-number');
-
-    // Assinging a random point value to each crystal at the start of each game.
-    var rubyNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-    var rubyGuess = rubyNum[Math.floor(Math.random() * rubyNum.length)];
-    var rubyNumDom = $('#r-num-test'); // --> This sets me up to show the gem point value for reference during developement. Will hide when game ios deployed.
-
-    var diamondNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-    var diamondGuess = diamondNum[Math.floor(Math.random() * diamondNum.length)];
-    var diamondNumDom = $('#d-num-test'); // --> This sets me up to show the gem point value for reference during developement. Will hide when game ios deployed.
-
-    var yellowNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-    var yellowGuess = rubyNum[Math.floor(Math.random() * yellowNum.length)];
-    var yellowNumDom = $('#y-num-test'); // --> This sets me up to show the gem point value for reference during developement. Will hide when game ios deployed.
-
-    var emeraldNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-    var emeraldGuess = rubyNum[Math.floor(Math.random() * emeraldNum.length)];
-    var emeraldNumDom = $('#e-num-test'); // --> This sets me up to show the gem point value for reference during developement. Will hide when game ios deployed.
-
-    // Delcaring local variables.
-    var wins = $('#wins-placeholder');
-    var losses = $('#losses-placeholder');
-    var totalScoreDom = $('#total-score');
-    var winPoints = 0;
-    var lossPoints = 0;
-    var restart = 0;
-    var counter = 0;
-    var targetNumber = computerGuess
-
-
-    // This sets the "Your Total Score Is:" from the DOM at 0 at the start of each game.
-    // totalScoreDom.text(restart);
-    // This sets the "Wins:" from the DOM at 0 at the start of each game.
-    wins.text(restart);
-    // This sets the "Losses:" from the DOM at 0 at the start of each game.
-    losses.text(restart);
-    // These set the random numbers chosen from arrays above to the selected targets on the DOM. I added the crystals' numbers so I can reference them when building. I sill change the css display to none when deploying the final product so the user won't see them.
-    computerNumDom.text(targetNumber);
-    rubyNumDom.text(rubyGuess);
-    diamondNumDom.text(diamondGuess);
-    yellowNumDom.text(yellowGuess);
-    emeraldNumDom.text(emeraldGuess);
-
     // Assiging a point value attribute to I can map the random number generated above for each gem.
     ruby.attr('rubyValue', rubyGuess);
     diamond.attr('diamondValue', diamondGuess);
@@ -70,23 +67,17 @@ $(document).ready(function() {
       totalScoreDom.text(counter);
 
       if (counter === targetNumber) {
-        alert('You Won!')
-          // location.reload();
+        alert('You won!')
         counter = 0;
-        totalScoreDom.text(counter);
-        playGame()
         winPoints++;
         wins.text(winPoints);
-
+        totalScoreDom.text(counter);
       } else if (counter > targetNumber) {
         alert('You Loss!')
-          // location.reload();
         counter = 0;
-        totalScoreDom.text(counter);
-        playGame()
         lossPoints++;
         losses.text(lossPoints);
-
+        totalScoreDom.text(counter);
       }
     })
 
@@ -97,17 +88,17 @@ $(document).ready(function() {
       totalScoreDom.text(counter);
 
       if (counter === targetNumber) {
-        alert('You Won!')
+        alert('You won!')
+        counter = 0;
         winPoints++;
         wins.text(winPoints);
-        location.reload();
-
-        playGame();
+        totalScoreDom.text(counter);
       } else if (counter > targetNumber) {
         alert('You Loss!')
+        counter = 0;
         lossPoints++;
         losses.text(lossPoints);
-        location.reload();
+        totalScoreDom.text(counter);
       }
     })
 
@@ -118,16 +109,17 @@ $(document).ready(function() {
       totalScoreDom.text(counter);
 
       if (counter === targetNumber) {
-        alert('You Won!')
+        alert('You won!')
+        counter = 0;
         winPoints++;
         wins.text(winPoints);
-        location.reload();
-
+        totalScoreDom.text(counter);
       } else if (counter > targetNumber) {
         alert('You Loss!')
+        counter = 0;
         lossPoints++;
         losses.text(lossPoints);
-        location.reload();
+        totalScoreDom.text(counter);
       }
     })
 
@@ -138,23 +130,24 @@ $(document).ready(function() {
       totalScoreDom.text(counter);
 
       if (counter === targetNumber) {
-        alert('You Won!')
+        alert('You won!')
+        counter = 0;
         winPoints++;
         wins.text(winPoints);
-        location.reload();
-
+        totalScoreDom.text(counter);
       } else if (counter > targetNumber) {
         alert('You Loss!')
+        counter = 0;
         lossPoints++;
         losses.text(lossPoints);
-        location.reload();
+        totalScoreDom.text(counter);
       }
     })
-
-
   }
 
   playGame();
+
+
 
 
 });
